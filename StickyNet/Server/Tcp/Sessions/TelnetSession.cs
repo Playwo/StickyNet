3115755949f5677b5f591173fca38d0e193ca89b
Playwo <eliaswolf2001@t-online.de>
2019-11-06 20:33:17 +0100

    Fixed Telnet

--- StickyNet/Server/Tcp/Sessions/TelnetSession.cs
+++ StickyNet/Server/Tcp/Sessions/TelnetSession.cs
@@ -1,4 +1,5 @@
-﻿using NetCoreServer;
+﻿using System.Text;
+using NetCoreServer;
 
 namespace StickyNet.Server.Tcp
 {
@@ -11,25 +12,38 @@ namespace StickyNet.Server.Tcp
         {
         }
 
-        protected override void OnConnected() => SendAsync("Welcome to Telnet! Please authenticate!\n");
-        protected override void OnTimeouted() => SendAsync("Timeouted...");
+        protected override void OnConnected() => SendAsync("Welcome to Telnet! Please authenticate!\r\n");
+        protected override void OnTimeouted() => SendAsync("Timeouted...\r\n");
         protected override void OnReceived(byte[] buffer, long offset, long size)
         {
+            ResetTimeout();
+
+            string message = Encoding.UTF8.GetString(buffer, (int) offset, (int) size);
+
             if (RemainingTries > 0)
             {
-                SendAsync("Invalid password!\n");
-
-                if (size > 100)
-                {
-                    SendAsync("Exceeded maximum!");
-                    Disconnect();
-                }
-
+                ProcessReceived(message);
                 RemainingTries--;
             }
+            if (RemainingTries == 0)
+            {
+                SendAsync("Out of authentication tries!\r\n");
+                Disconnect();
+            }
+        }
 
-            SendAsync("Out of authentication tries!\n");
-            Disconnect();
+        private void ProcessReceived(string received)
+        {
+            if (received.Contains("\r\n"))
+            {
+                SendAsync("Invalid password!\r\n");
+
+                if (received.Length > 100)
+                {
+                    SendAsync("Exceeded maximum!\r\n");
+                    Disconnect();
+                }
+            }
         }
     }
 }
